61acef5e0ffc5281a21453d3e2daf904f612c47b
Mikxy <52295818+Mikxy@users.noreply.github.com>
2019-06-27 19:46:34 +0100

    Synapse X API

---
+++ AJax/AJax/AJax.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace AJax
+{
+    partial class AJax
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AJax));
+            this.Tools = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDropDownButton4 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton5 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Scriptbox = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.Scriptlist = new System.Windows.Forms.ListBox();
+            this.Tools.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Scriptbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tools
+            // 
+            this.Tools.CanOverflow = false;
+            this.Tools.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.Tools.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripDropDownButton4,
+            this.toolStripDropDownButton5,
+            this.toolStripDropDownButton1});
+            this.Tools.Location = new System.Drawing.Point(0, 0);
+            this.Tools.Name = "Tools";
+            this.Tools.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.Tools.ShowItemToolTips = false;
+            this.Tools.Size = new System.Drawing.Size(484, 25);
+            this.Tools.TabIndex = 1;
+            this.Tools.Text = "Tools";
+            this.Tools.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Tools_ItemClicked);
+            this.Tools.MouseDown += new System.Windows.Forms.MouseEventHandler(this.formdrag_mousedown);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(40, 22);
+            this.toolStripButton1.Text = "Inject";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripDropDownButton4
+            // 
+            this.toolStripDropDownButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton4.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem8,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem5});
+            this.toolStripDropDownButton4.ForeColor = System.Drawing.Color.Black;
+            this.toolStripDropDownButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton4.Name = "toolStripDropDownButton4";
+            this.toolStripDropDownButton4.Size = new System.Drawing.Size(69, 22);
+            this.toolStripDropDownButton4.Text = "Scriptbox";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(153, 22);
+            this.toolStripMenuItem6.Text = "Submit Script";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(153, 22);
+            this.toolStripMenuItem8.Text = "Load Script";
+            this.toolStripMenuItem8.Click += new System.EventHandler(this.toolStripMenuItem8_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(153, 22);
+            this.toolStripMenuItem7.Text = "Save Script";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem7_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(153, 22);
+            this.toolStripMenuItem5.Text = "Clear Scriptbox";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // toolStripDropDownButton5
+            // 
+            this.toolStripDropDownButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton5.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem2});
+            this.toolStripDropDownButton5.ForeColor = System.Drawing.Color.Black;
+            this.toolStripDropDownButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton5.Name = "toolStripDropDownButton5";
+            this.toolStripDropDownButton5.Size = new System.Drawing.Size(62, 22);
+            this.toolStripDropDownButton5.Text = "Settings";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(165, 22);
+            this.toolStripMenuItem4.Text = "Topmost = null";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(165, 22);
+            this.toolStripMenuItem2.Text = "WordWrap = null";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click_2);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3});
+            this.toolStripDropDownButton1.ForeColor = System.Drawing.Color.Black;
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(50, 22);
+            this.toolStripDropDownButton1.Text = "Extras";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Text = "Scripts Folder = nil";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // Scriptbox
+            // 
+            this.Scriptbox.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.Scriptbox.AutoIndentCharsPatterns = "\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>.+)\n";
+            this.Scriptbox.AutoScrollMinSize = new System.Drawing.Size(0, 13);
+            this.Scriptbox.BackBrush = null;
+            this.Scriptbox.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            this.Scriptbox.CharHeight = 13;
+            this.Scriptbox.CharWidth = 7;
+            this.Scriptbox.CommentPrefix = "--";
+            this.Scriptbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Scriptbox.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.Scriptbox.Font = new System.Drawing.Font("Courier New", 9F);
+            this.Scriptbox.IsReplaceMode = false;
+            this.Scriptbox.Language = FastColoredTextBoxNS.Language.Lua;
+            this.Scriptbox.LeftBracket = '(';
+            this.Scriptbox.LeftBracket2 = '{';
+            this.Scriptbox.LineNumberColor = System.Drawing.Color.Black;
+            this.Scriptbox.Location = new System.Drawing.Point(0, 25);
+            this.Scriptbox.Name = "Scriptbox";
+            this.Scriptbox.Paddings = new System.Windows.Forms.Padding(0);
+            this.Scriptbox.RightBracket = ')';
+            this.Scriptbox.RightBracket2 = '}';
+            this.Scriptbox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.Scriptbox.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("Scriptbox.ServiceColors")));
+            this.Scriptbox.Size = new System.Drawing.Size(484, 236);
+            this.Scriptbox.TabIndex = 2;
+            this.Scriptbox.WordWrap = true;
+            this.Scriptbox.Zoom = 100;
+            this.Scriptbox.TextChanged += new System.EventHandler<FastColoredTextBoxNS.TextChangedEventArgs>(this.Scriptbox_TextChanged);
+            this.Scriptbox.Load += new System.EventHandler(this.Scriptbox_Load);
+            // 
+            // Scriptlist
+            // 
+            this.Scriptlist.BackColor = System.Drawing.Color.White;
+            this.Scriptlist.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Scriptlist.FormattingEnabled = true;
+            this.Scriptlist.IntegralHeight = false;
+            this.Scriptlist.Location = new System.Drawing.Point(484, 25);
+            this.Scriptlist.Name = "Scriptlist";
+            this.Scriptlist.Size = new System.Drawing.Size(158, 236);
+            this.Scriptlist.TabIndex = 3;
+            this.Scriptlist.SelectedIndexChanged += new System.EventHandler(this.Scriptlist_SelectedIndexChanged);
+            // 
+            // AJax
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.Scriptbox);
+            this.Controls.Add(this.Tools);
+            this.Controls.Add(this.Scriptlist);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "AJax";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AJax - Idle";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form4_FormClosing);
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.Tools.ResumeLayout(false);
+            this.Tools.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Scriptbox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip Tools;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton4;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton5;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private FastColoredTextBoxNS.FastColoredTextBox Scriptbox;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ListBox Scriptlist;
+    }
+}
---
+++ AJax/AJax/AJax.cs
@@ -0,0 +1,385 @@
+﻿using sxlib;
+using sxlib.Specialized;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AJax
+{
+    public partial class AJax : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void formdrag_mousedown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+
+        public AJax()
+        {
+            InitializeComponent();
+        }
+
+        bool a = false;
+
+        private async void Form4_Load(object sender, EventArgs e)
+        {
+            TUpdate("Loading");
+            Tools.Enabled = false;
+            Scriptbox.Enabled = false;
+            Scriptlist.Enabled = false;
+
+            try
+            {
+                foreach (string path in Directory.GetFiles("./Scripts"))
+                {
+                    this.Scriptlist.Items.Add(Path.GetFileName(path));
+                }
+            }
+            catch (Exception)
+            {
+                Directory.CreateDirectory("./Scripts");
+                foreach (string path2 in Directory.GetFiles("./Scripts"))
+                {
+                    this.Scriptlist.Items.Add(Path.GetFileName(path2));
+                }
+
+            }
+
+            try
+            {
+                a = Convert.ToBoolean(new Settings().SettingsConfig.Read("ScriptsFolder", "Configuration"));
+
+                if (a == false)
+                {
+                    this.Size = new Size(500, 300);
+                }
+                else
+                {
+                    this.Size = new Size(658, 300);
+                }
+                
+            }
+            catch (Exception)
+            {
+
+            }
+
+            toolStripMenuItem3.Text = "Scripts Folder = " + a.ToString();
+
+            if (File.Exists("Settings.AJax"))
+            {
+                string tm = new Settings().SettingsConfig.Read("Topmost", "Configuration");
+                TopMost = Convert.ToBoolean(tm);
+                toolStripMenuItem4.Text = "Topmost = " + TopMost.ToString();
+
+                string wr = new Settings().SettingsConfig.Read("WordWrap", "Configuration");
+                Scriptbox.WordWrap = Convert.ToBoolean(wr);
+                toolStripMenuItem2.Text = "WordWrap = " + Scriptbox.WordWrap.ToString();
+                Scriptbox.Text = File.ReadAllText(@"bin\\AJax.lua");
+
+            }
+            else
+            {
+                toolStripMenuItem4.Text = "Topmost = True";
+                toolStripMenuItem2.Text = "WordWrap = False";
+            }
+
+            if (File.Exists(@"bin\\AJax.lua"))
+            {
+                await Task.Delay(500);
+                Scriptbox.Text = File.ReadAllText(@"bin\\AJax.lua");
+            }
+
+            Synapse = SxLib.InitializeOffscreen(directory);
+            Synapse.LoadEvent += Loader;
+            Synapse.AttachEvent += Attacher;
+            Synapse.Load();
+
+        }
+
+        private void Form4_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are you sure?", "AJax - Close Confirmation", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                TUpdate("Closing");
+                Environment.Exit(-1);
+            }
+        }
+
+        private void toolStripDropDownButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+            a = !a;
+            if (a == false)
+            {
+                this.Size = new Size(500, 300);
+            }
+            else
+            {
+                this.Size = new Size(658, 300);
+            }
+
+            new Settings().SettingsConfig.Write("ScriptsFolder", a.ToString(), "Configuration");
+            toolStripMenuItem3.Text = "Scripts Folder = " + a.ToString();
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+
+            TopMost = !TopMost;
+            new Settings().SettingsConfig.Write("Topmost", TopMost.ToString(), "Configuration");
+            toolStripMenuItem4.Text = "Topmost = " + TopMost.ToString();
+        }
+
+        void TUpdate(string text)
+        {
+            this.Text = "Ajax - " + text;
+        }
+
+        async void RUpdate()
+        {
+            await Task.Delay(3000);
+            this.Text = "Ajax - Idle";
+        }
+
+
+        private void toolStripMenuItem2_Click_2(object sender, EventArgs e)
+        {
+            Scriptbox.WordWrap = !Scriptbox.WordWrap;
+            new Settings().SettingsConfig.Write("WordWrap", Scriptbox.WordWrap.ToString(), "Configuration");
+            toolStripMenuItem2.Text = "WordWrap = " + Scriptbox.WordWrap.ToString();
+        }
+
+        public static SxLibOffscreen Synapse;
+        
+        void Attacher(SxLibBase.SynAttachEvents Event)
+        {
+            switch (Event)
+            {
+                case SxLibBase.SynAttachEvents.READY:
+                    {
+                        TUpdate("Injected");
+                        Tools.Enabled = true;
+                        RUpdate();
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.FAILED_TO_FIND:
+                    {
+                        TUpdate("Roblox not found");
+                        Tools.Enabled = true;
+                        RUpdate();
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.SCANNING:
+                    {
+                        TUpdate("Scanning");
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.INJECTING:
+                    {
+                        TUpdate("Injecting");
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.CHECKING_WHITELIST:
+                    {
+                        TUpdate("Checking whitelist");
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.CHECKING:
+                    {
+                        TUpdate("Checking Roblox");
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.NOT_RUNNING_LATEST_VER:
+                    {
+                        TUpdate("SynX UI is outdated");
+                        Tools.Enabled = true;
+                        RUpdate();
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.ALREADY_INJECTED:
+                    {
+                        TUpdate("Already Injected");
+                        Tools.Enabled = true;
+                        RUpdate();
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.NOT_INJECTED:
+                    {
+                        TUpdate("Inject first");
+                        Tools.Enabled = true;
+                        RUpdate();
+                        break;
+                    }
+
+                case SxLibBase.SynAttachEvents.PROC_DELETION:
+                    {
+                        TUpdate("Disconnected from Roblox");
+                        RUpdate();
+                        break;
+                    }
+            }
+        }
+
+        void Loader(SxLibBase.SynLoadEvents Event)
+        {
+            switch (Event)
+            {
+                case SxLibBase.SynLoadEvents.READY:
+                    {
+
+                        TUpdate("Idle");
+                        Tools.Enabled = true;
+                        Scriptbox.Enabled = true;
+                        Scriptlist.Enabled = true;
+                        //Synapse.Attach();
+                        break;
+                    }
+
+                case SxLibBase.SynLoadEvents.NOT_UPDATED:
+                    {
+                        TUpdate("Wait for Roblox Update");
+                        break;
+                    }
+            }
+        }
+
+        public static string directory = Directory.GetCurrentDirectory();
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            Synapse.Execute(Scriptbox.Text);
+            File.WriteAllText(@"bin\\AJax.lua", Scriptbox.Text);
+        }
+
+        private void Scriptbox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Scriptbox_TextChanged(object sender, FastColoredTextBoxNS.TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            Scriptbox.Text = "";
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "AJax Save Script";
+            sfd.Filter = "Lua Script (*.lua*)|*.lua*";
+            sfd.ShowDialog();
+            try
+            {
+                string script = Scriptbox.Text;
+                File.WriteAllText(sfd.FileName, Scriptbox.Text);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void toolStripMenuItem8_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "AJax Open Script";
+            ofd.Filter = "Lua Script (*.lua*)|*.lua*";
+            ofd.ShowDialog();
+            try
+            {
+                string file = File.ReadAllText(ofd.FileName);
+                Scriptbox.Text = file;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            TUpdate("Loading");
+            Tools.Enabled = false;
+            try
+            {
+                Synapse.Attach();
+            }
+            catch (Exception)
+            {
+                Synapse.Load();
+            }
+        }
+
+        private void Scriptlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = File.ReadAllText("./Scripts/" + this.Scriptlist.SelectedItems[0]);
+                this.Scriptbox.Text = text;
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    string text2 = File.ReadAllText("./Scripts/" + this.Scriptlist.SelectedItems[0]);
+                    this.Scriptbox.Text = text2;
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        private void Tools_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
